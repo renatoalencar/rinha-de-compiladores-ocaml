4819a9e12cbc97879ddcb5c79f673493ab515139
Renato Alencar <renatoalencar.73@gmail.com>
2023-09-12 08:56:54 -0300

    Allow using CLANG and flags from environment variables

--- bin/main.ml
+++ bin/main.ml
@@ -4,7 +4,10 @@ let main () =
   let input_filename = Sys.argv.(1) in
 
   let parsetree =
-    let in_channel = open_in input_filename in
+    let in_channel =
+      if input_filename = "-" then stdin
+      else open_in input_filename
+    in
     let parsetree = Json.of_channel in_channel in
     close_in in_channel;
     parsetree
@@ -17,9 +20,22 @@ let main () =
   Codegen.compile_main output typedtree;
   close_out output;
 
-  ignore @@
-    Sys.command
-      (String.concat " "
-        [ "clang -o"; Sys.argv.(2); "runtime.o"; output_filename ])
+  let clang =
+    match Sys.getenv_opt "CLANG" with
+    | Some clang -> clang
+    | None -> "clang"
+  in
+  let flags =
+    match Sys.getenv_opt "CLANG_FLAGS" with
+    | Some flags -> flags
+    | None -> ""
+  in
+
+  let command =
+    String.concat " "
+        [ clang; flags; "-o"; Sys.argv.(2); "runtime.o"; output_filename ]
+  in
+  prerr_endline command;
+  exit @@ Sys.command command
 
 let () = main ()
